4137891a41b89a55035a689392ece08bbedc5a83
Ryan Cather <ryan.cather@ed.act.edu.au>
2022-08-27 23:09:55 +1000

    added temperature & LCD functionality

--- RMS/RMS.ino
+++ RMS/RMS.ino
@@ -2,6 +2,33 @@
 
 #define FORMAT_SPIFFS_IF_FAILED true
 
+/***************************************************
+  This is a library for the Adafruit 0.96" Mini TFT Featherwing
+  
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ ****************************************************/
+/**************************************************************************/
+/*!
+  This is a demo for the Adafruit ADT7410 breakout
+  ----> http://www.adafruit.com/products/4089
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+*/
+/**************************************************************************/
+
+#include <Wire.h>
+#include "Adafruit_ADT7410.h"
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include "Adafruit_miniTFTWing.h"
+
+
 // Wifi & Webserver
 #include "WiFi.h"
 #include "SPIFFS.h"
@@ -10,6 +37,15 @@
 
 AsyncWebServer server(80);
 
+Adafruit_miniTFTWing ss;
+#define TFT_RST    -1    // we use the seesaw for resetting to save a pin
+#define TFT_CS   14
+#define TFT_DC   32
+
+// Create the ADT7410 temperature sensor object
+Adafruit_ADT7410 tempsensor = Adafruit_ADT7410();
+
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 
 // RTC Start - Remove if unnecessary
 #include "RTClib.h"
@@ -36,6 +72,34 @@ void setup() {
     Serial.println("SPIFFS Mount Failed");
     return;
   }
+
+  if (!ss.begin()) {
+    Serial.println("seesaw init error!");
+    while (1);
+  }
+  else Serial.println("seesaw started");
+
+  ss.tftReset();
+  ss.setBacklight(0x0); //set the backlight fully on
+
+  // Use this initializer (uncomment) if you're using a 0.96" 180x60 TFT
+  tft.initR(INITR_MINI160x80);   // initialize a ST7735S chip, mini display
+
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+
+  
+  Serial.println("ADT7410 demo");
+
+  // Make sure the sensor is found, you can also pass in a different i2c
+  // address with tempsensor.begin(0x49) for example
+  if (!tempsensor.begin()) {
+    Serial.println("Couldn't find ADT7410!");
+    while (1);
+  }
+
+  // sensor takes 250 ms to get first readings
+  delay(250);
 
   // Wifi Configuration
   WiFi.begin(ssid, password);
@@ -67,10 +131,8 @@ void setup() {
 }
 
 void loop() {
-
   builtinLED();
-
-
+  updateTemperature();
   delay(LOOPDELAY); // To allow time to publish new code.
 }
 
@@ -104,4 +166,25 @@ void logEvent(String dataToLog) {
 
   Serial.print("\nEvent Logged: ");
   Serial.println(logEntry);
+}
+
+
+void tftDrawText(String text, uint16_t color) {
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 0);
+  tft.setTextSize(3);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
+void updateTemperature() {
+  // Read and print out the temperature, then convert to *F
+  float c = tempsensor.readTempC();
+  float f = c * 9.0 / 5.0 + 32;
+  Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t");
+  Serial.print(f); Serial.println("*F");
+  String tempInC = String(c);
+  tftDrawText(tempInC, ST77XX_WHITE);
+  delay(100);
 }
