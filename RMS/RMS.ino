9d56c27fb4df0a55a362239c4d513c7fc7af39da
Ryan Cather <ryan.cather@ed.act.edu.au>
2022-08-31 20:47:29 +1000

    reset to beginning code.

--- RMS/RMS.ino
+++ RMS/RMS.ino
@@ -1,40 +1,6 @@
-/* Welcome to the code base for the Remote Monitoring System (RMS) (c) 2022
-
-   This code is developed for the Adafruit Feather Huzzah32 and runs a webserver for users to access.
-
-   THIS IS THE WORK IN PROGRESS (WIP) Branch! NOT Production
-*/
-
 #include "sensitiveInformation.h"
 
 #define FORMAT_SPIFFS_IF_FAILED true
-
-/***************************************************
-  This is a library for the Adafruit 0.96" Mini TFT Featherwing
-  
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.
-  MIT license, all text above must be included in any redistribution
- ****************************************************/
-/**************************************************************************/
-/*!
-  This is a demo for the Adafruit ADT7410 breakout
-  ----> http://www.adafruit.com/products/4089
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit and open-source hardware by purchasing
-  products from Adafruit!
-*/
-/**************************************************************************/
-
-#include <Wire.h>
-#include "Adafruit_ADT7410.h"
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
-#include "Adafruit_miniTFTWing.h"
-
 
 // Wifi & Webserver
 #include "WiFi.h"
@@ -44,46 +10,14 @@
 
 AsyncWebServer server(80);
 
-// DC Motor Featherwing Start
-
-#include <Wire.h>
-#include <Adafruit_MotorShield.h>
-//#include "utility/Adafruit_PWMServoDriver.h"
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-
-// DC Motor Featherwing End
-
-Adafruit_miniTFTWing ss;
-#define TFT_RST    -1    // we use the seesaw for resetting to save a pin
-#define TFT_CS   14
-#define TFT_DC   32
-
-// Create the ADT7410 temperature sensor object
-Adafruit_ADT7410 tempsensor = Adafruit_ADT7410();
-
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 
 // RTC Start - Remove if unnecessary
 #include "RTClib.h"
 
 RTC_PCF8523 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 // RTC End
-
-// MiniTFT Start
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
-#include "Adafruit_miniTFTWing.h"
-
-Adafruit_miniTFTWing ss;
-#define TFT_RST    -1     // we use the seesaw for resetting to save a pin
-#define TFT_CS   14       // THIS IS DIFFERENT FROM THE DEFAULT CODE
-#define TFT_DC   32       // THIS IS DIFFERENT FROM THE DEFAULT CODE
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
-
-
-// MiniTFT End
 
 boolean LEDOn = false; // State of Built-in LED true=on, false=off.
 #define LOOPDELAY 100
@@ -102,34 +36,6 @@ void setup() {
     Serial.println("SPIFFS Mount Failed");
     return;
   }
-
-  if (!ss.begin()) {
-    Serial.println("seesaw init error!");
-    while (1);
-  }
-  else Serial.println("seesaw started");
-
-  ss.tftReset();
-  ss.setBacklight(0x0); //set the backlight fully on
-
-  // Use this initializer (uncomment) if you're using a 0.96" 180x60 TFT
-  tft.initR(INITR_MINI160x80);   // initialize a ST7735S chip, mini display
-
-  tft.setRotation(3);
-  tft.fillScreen(ST77XX_BLACK);
-
-  
-  Serial.println("ADT7410 demo");
-
-  // Make sure the sensor is found, you can also pass in a different i2c
-  // address with tempsensor.begin(0x49) for example
-  if (!tempsensor.begin()) {
-    Serial.println("Couldn't find ADT7410!");
-    while (1);
-  }
-
-  // sensor takes 250 ms to get first readings
-  delay(250);
 
   // Wifi Configuration
   WiFi.begin(ssid, password);
@@ -152,54 +58,19 @@ void setup() {
     Serial.flush();
     //    abort();
   }
-  if (! rtc.initialized() || rtc.lostPower()) {
-    logEvent("RTC is NOT initialized, let's set the time!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
 
+  // The following line can be uncommented if the time needs to be reset.
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   rtc.start();
-
-  // RTC End
-
-
-  // MiniTFT Start
-  if (!ss.begin()) {
-    logEvent("seesaw init error!");
-    while (1);
-  }
-  else logEvent("seesaw started");
-
-  ss.tftReset();
-  ss.setBacklight(0x0); //set the backlight fully on
-
-  // Use this initializer (uncomment) if you're using a 0.96" 180x60 TFT
-  tft.initR(INITR_MINI160x80);   // initialize a ST7735S chip, mini display
-
-  tft.setRotation(3);
-  tft.fillScreen(ST77XX_BLACK);
-
-  // MiniTFT End
-
   pinMode(LED_BUILTIN, OUTPUT);
-  logEvent(listFiles(false));
-
-  // DC Motor Featherwing Start
-
-  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
-    // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
-    logEvent("Could not find Motor Shield. Check wiring.");
-    while (1);
-  }
-  logEvent("Motor Shield found.");
-  myMotor->setSpeed(100);
-
-  // DC Motor Featherwing End
 
 }
 
 void loop() {
+
   builtinLED();
-  updateTemperature();
+
+
   delay(LOOPDELAY); // To allow time to publish new code.
 }
 
@@ -233,25 +104,4 @@ void logEvent(String dataToLog) {
 
   Serial.print("\nEvent Logged: ");
   Serial.println(logEntry);
-}
-
-
-void tftDrawText(String text, uint16_t color) {
-  tft.fillScreen(ST77XX_BLACK);
-  tft.setCursor(0, 0);
-  tft.setTextSize(3);
-  tft.setTextColor(color);
-  tft.setTextWrap(true);
-  tft.print(text);
-}
-
-void updateTemperature() {
-  // Read and print out the temperature, then convert to *F
-  float c = tempsensor.readTempC();
-  float f = c * 9.0 / 5.0 + 32;
-  Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t");
-  Serial.print(f); Serial.println("*F");
-  String tempInC = String(c);
-  tftDrawText(tempInC, ST77XX_WHITE);
-  delay(100);
 }
--- RMS/websiteFunctionality.ino
+++ RMS/websiteFunctionality.ino
@@ -23,29 +23,17 @@ void routesConfiguration() {
   // Example of a route with additional authentication (popup in browser)
   // And uses the processor function.
   server.on("/dashboard.html", HTTP_GET, [](AsyncWebServerRequest * request) {
-    if (!request->authenticate(usernameGuest, passwordGuest))
+    if (!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
     logEvent("Dashboard");
-    writeTextTFT("Guest", ST77XX_GREEN, 4);
     request->send(SPIFFS, "/dashboard.html", "text/html", false, processor);
   });
-
-  // Example of a route with additional authentication (popup in browser)
-  // And uses the processor function.
-  server.on("/admin", HTTP_GET, [](AsyncWebServerRequest * request) {
-    if (!request->authenticate(usernameAdmin, passwordAdmin))
-      return request->requestAuthentication();
-    logEvent("Administrator Access");
-    writeTextTFT("Admin", ST77XX_RED, 4);
-    request->send(SPIFFS, "/admin.html", "text/html", false, processor);
-  });
-
 
 
   // Example of route with authentication, and use of processor
   // Also demonstrates how to have arduino functionality included (turn LED on)
   server.on("/LEDOn", HTTP_GET, [](AsyncWebServerRequest * request) {
-    if ((!request->authenticate(usernameGuest, passwordGuest) && (!request->authenticate(usernameAdmin, passwordAdmin)) ))
+    if (!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
     LEDOn = true;
     request->send(SPIFFS, "/dashboard.html", "text/html", false, processor);
@@ -53,7 +41,7 @@ void routesConfiguration() {
 
 
   server.on("/LEDOff", HTTP_GET, [](AsyncWebServerRequest * request) {
-    if ((!request->authenticate(usernameGuest, passwordGuest) && (!request->authenticate(usernameAdmin, passwordAdmin)) ))
+    if (!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
     LEDOn = false;
     request->send(SPIFFS, "/dashboard.html", "text/html", false, processor);
@@ -62,12 +50,11 @@ void routesConfiguration() {
 
   // Example of route which sets file to download - 'true' in send() command.
   server.on("/logOutput", HTTP_GET, [](AsyncWebServerRequest * request) {
-    if (!request->authenticate(usernameAdmin, passwordAdmin))
+    if (!request->authenticate(http_username, http_password))
       return request->requestAuthentication();
     logEvent("Log Event Download");
     request->send(SPIFFS, "/logEvents.csv", "text/html", true);
   });
-
 }
 
 String getDateTime() {
@@ -89,10 +76,6 @@ String processor(const String& var) {
   if (var == "DATETIME") {
     String datetime = getDateTime();
     return datetime;
-  }
-
-  if (var == "FILELIST") {
-    return listFiles(true);
   }
 
   // Default "catch" which will return nothing in case the HTML has no variable to replace.
